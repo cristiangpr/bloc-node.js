82785c63ce8c56fee0758faab2773d8f025840e1
Cristian Perez <cristiangp777@gmail.com>
2019-01-30 10:32:55 -0800

    node greeting app

---
+++ node-greeting-app/main.js
@@ -0,0 +1,21 @@
+const greetings = require("./node-greeting.js");
+const goodbyes = require("./node-goodbyes.js");
+
+
+greetings.greetingEnglish();
+
+
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+
+greetings.greetingIcelandic();
+
+goodbyes.goodbyeEnglish();
+
+
+goodbyes.goodbyeJapanese();
+
+goodbyes.goodbyeSpanish();
+
+goodbyes.goodbyeIcelandic();
